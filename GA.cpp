0503c9b46e94cb66976f49373b527f0a80f2da4a
TzuChieh <james093131@gmail.com>
2020-11-22 14:54:24 +0800

    CX_Finish

--- GA.cpp
+++ GA.cpp
@@ -4,7 +4,14 @@ int main(int argc, char *argv[])
     srand(time(NULL));
     int pop = atoi(argv[1]);
 	int iteration = atoi(argv[2]);
-    int run = atoi(argv[3]);    
-    RUN(iteration,pop,run);
+    int run = atoi(argv[3]);
+    if (argc > 3)
+    {
+        RUN(iteration,pop,run);
+    }
+    else
+    {
+        RUN(1000,30,30);
+    }    
    
 }
